1e1f8f40558db4dd98279c41a5a792db14b73ad9
WHD <e19880910@gmail.com>
2017-06-14 01:01:30 +0800

    建立單元測試和被測類別與其方法。

---
+++ TDD_SumDataList/TDD_SumDataList/SumDataListGenerater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_SumDataList
+{
+	public class SumDataListGenerater
+	{
+		public IList<int> GetSumDataList<T>(IList<T> sourceData)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
---
+++ TDD_SumDataList/TDD_SumDataListTests/SumDataListGeneraterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_SumDataList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_SumDataList.Tests
+{
+	[TestClass()]
+	public class SumDataListGeneraterTests
+	{
+		[TestMethod()]
+		public void GetSumDataListTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
